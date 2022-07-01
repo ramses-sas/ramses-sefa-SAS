83b350d51e0320437f56361e77583e05adf743dc
Giancarlo Sorrentino <57813140+SirGian99@users.noreply.github.com>
2022-07-01 21:03:16 +0200

    Gateway funzionante

---
+++ API-Gateway-service/src/main/java/polimi/saefa/apigateway/APIGatewayApplication.java
@@ -0,0 +1,11 @@
+package polimi.saefa.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(APIGatewayApplication.class, args);
+    }
+}
