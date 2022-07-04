675ecfe930e2520302ce515d9824c10a28967b02
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-07-04 15:50:34 +0200

    Update docker scripts

--- restaurant-service/dockerBuild.sh
+++ restaurant-service/dockerBuild.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Syntax: dockerBuild [-p|r|t|h]"
+   echo "Options:"
+   echo "p     Select port to run on. Defaults to 8080."
+   echo "r     Use a random port to run on."
+   echo "t     Specify target platform for the container. Defaults to linux/arm64/v8."
+   echo "h     Display help."
+   echo
+}
+
 SERVICE_NAME=`awk -v FS="spring.application.name=" 'NF>1{print $2}' ./src/main/resources/application.properties`
 if [ "$SERVICE_NAME" = "" ]; then
     SERVICE_NAME="unknown-service"
@@ -11,20 +27,27 @@ fi
 
 PORT="8080"
 PORT_OPTION="${PORT}:${SERVICE_PORT}"
-while getopts "p:r" option; do
+TARGET="linux/arm64/v8"
+while getopts "p:rht:" option; do
    case $option in
+      h) # display Help
+        Help
+        exit;;
       p) # Port
         PORT_OPTION="${OPTARG}:${SERVICE_PORT}";;
       r) # Random port
         PORT_OPTION="${SERVICE_PORT}"
         PORT="";;
+      t) # Target
+        TARGET="${OPTARG}";;
      \?) # Wrong option
-         echo "UNKNOWN OPTION";;
+         echo "UNKNOWN OPTION $option"
+         exit;;
    esac
 done
 
 ../gradlew build
-docker build -t $SERVICE_NAME .
+docker build --platform=$TARGET -t $SERVICE_NAME .
 docker stop $SERVICE_NAME
 docker rm $SERVICE_NAME
 docker create -p $PORT_OPTION -i -t --name $SERVICE_NAME $SERVICE_NAME
