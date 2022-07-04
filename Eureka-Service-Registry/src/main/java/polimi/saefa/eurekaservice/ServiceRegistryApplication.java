83045a209a9fd07992ae534e18d2dced4258feaf
Giancarlo Sorrentino <57813140+SirGian99@users.noreply.github.com>
2022-07-04 17:45:56 +0200

    Fixes

--- Eureka-Service-Registry/src/main/java/polimi/saefa/eurekaservice/ServiceRegistryApplication.java
+++ Eureka-Service-Registry/src/main/java/polimi/saefa/eurekaservice/ServiceRegistryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class ServiceRegisrtyApplication {
+public class ServiceRegistryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRegisrtyApplication.class, args);
+        SpringApplication.run(ServiceRegistryApplication.class, args);
     }
 }
