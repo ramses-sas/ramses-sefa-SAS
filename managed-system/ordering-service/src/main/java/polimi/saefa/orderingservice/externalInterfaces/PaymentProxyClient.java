ff65e5f7c8621a3b06108d00b188252c585bcf4e
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-08-13 16:57:29 +0200

    Unused imports

--- managed-system/ordering-service/src/main/java/polimi/saefa/orderingservice/externalInterfaces/PaymentProxyClient.java
+++ managed-system/ordering-service/src/main/java/polimi/saefa/orderingservice/externalInterfaces/PaymentProxyClient.java
@@ -1,7 +1,6 @@
 package polimi.saefa.orderingservice.externalInterfaces;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import polimi.saefa.paymentproxyservice.restapi.ProcessPaymentRequest;
