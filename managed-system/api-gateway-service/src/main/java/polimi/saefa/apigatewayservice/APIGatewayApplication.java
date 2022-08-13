ec61252dc7b6dfa56d06013a969779b3736ead91
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-08-13 16:57:57 +0200

    Update APIGatewayApplication.java

--- managed-system/api-gateway-service/src/main/java/polimi/saefa/apigatewayservice/APIGatewayApplication.java
+++ managed-system/api-gateway-service/src/main/java/polimi/saefa/apigatewayservice/APIGatewayApplication.java
@@ -1,11 +1,10 @@
 package polimi.saefa.apigatewayservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Slf4j
 @EnableDiscoveryClient
@@ -13,19 +12,7 @@ import java.util.regex.Pattern;
 public class APIGatewayApplication {
 
     public static void main(String[] args) {
-        //Pattern lbPattern = Pattern.compile("loadbalancing\\.([\\w-\\.])+\\.weight", Pattern.CASE_INSENSITIVE);
-        Pattern lbPattern = Pattern.compile("loadbalancing\\.([\\w\\d-.])+", Pattern.CASE_INSENSITIVE);
-        Matcher lbMatcher = lbPattern.matcher("loadbalancing.restaurant-service");
-        //Matcher lbMatcher = lbPattern.matcher("loadbalancing.restaurant-service.92.168.1.1-8888.weight");
-        /*Pattern serviceNamePattern = Pattern.compile("([\\w-])\\.", Pattern.CASE_INSENSITIVE);
-        for (MatchResult matchResult : matchResults) {
-            System.out.println(matchResult.group(0));
-        }*/
-        //boolean matchFound = matcher.results().toList().size() > 0;
-        while(lbMatcher.find()){
-            log.info("matchFound: {}", lbMatcher.group());
-        }
-        //SpringApplication.run(APIGatewayApplication.class, args);
+        SpringApplication.run(APIGatewayApplication.class, args);
     }
 
 
