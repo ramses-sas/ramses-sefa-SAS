f96c4db48f93c1e73b0868b8f99623d68a5d6b68
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-08-25 13:25:36 +0200

    Scrip docker aggiornati

--- managed-system/dockerRunAll.sh
+++ managed-system/dockerRunAll.sh
@@ -4,58 +4,88 @@ NC='\033[0m' # No Color
 
 PrintError() {
   echo -e "${RED}$1${NC}"
+  exit -1
 }
 
 PrintSuccess() {
   echo -e "${GREEN}$1${NC}"
 }
 
+docker ps >/dev/null 2>&1 || PrintError "Cannot connect to Docker daemon"
+
 Help()
 {
    # Display Help
-   echo "Syntax: setup [-h|e]"
-   echo "Run without options to run every microservice locally in a dedicated container using an external Eureka Service."
+   echo "Syntax: dockerRunAll [-h|r]"
+   echo "Run without options to run every microservice in local mode."
+   echo "     Only the microservices hosted on the same LAN can communicate with each other"
+   echo "     The addresses of the server microservices and of the API gateway are defined in the application.properties file of the config repository"
+   echo "     It does not run the Eureka Server, the MySQL Server and the Config Server"
    echo "Options:"
    echo "h     Display help."
-   echo "l     Run everything locally."
+   echo "r     Run the microservice in the remote mode."
+   echo "         It uses the public IP address of the machine, obtained through an external service. "
+   echo "         It usually requires a port forwarding for the microservice ports."
+   echo "         The addresses of the Eureka Server, of the MySQL Server and of the API Gateway are retrieved from the config repository"
 }
 
 SCRIPTS_PATH="$( cd "scripts" && pwd && cd .. )"
-HOST_IP=`ifconfig | grep '\<inet\>' | cut -d ' ' -f2 | grep -v '127.0.0.1'`
-EUREKA_IP_PORT="52.208.38.53:58082"
-MYSQL_SERVER="52.208.38.53"
+HOST=`ifconfig | grep '\<inet\>' | cut -d ' ' -f2 | grep -v '127.0.0.1'`
+IS_REMOTE="no"
 
-# EUREKA_IP_PORT=""
+
 while getopts "hl" option; do
    case $option in
       h) # display Help
         Help
         exit;;
-      l) # Run everything locally
-        cd "eureka-registry-service/" || return
-        EUREKA_IP_PORT="$HOST_IP:58082"
-        MYSQL_SERVER="$HOST_IP"
-        bash "$SCRIPTS_PATH/dockerBuild.sh" -e "$EUREKA_IP_PORT"
-        bash "$SCRIPTS_PATH/dockerRun.sh"
-        cd ..;;
+      r)
+        HOST="$(curl https://ipinfo.io/ip)"
+        IS_REMOTE="yes";;
      \?) # Wrong option
-        PrintError "UNKNOWN OPTION"
-        exit;;
+        PrintError "UNKNOWN OPTION";;
    esac
 done
+
+BuildNRun() {
+  if [ "$IS_REMOTE" = "no" ] ; then
+    bash "$SCRIPTS_PATH/dockerBuild.sh"; bash "$SCRIPTS_PATH/dockerRun.sh"
+  else
+    bash "$SCRIPTS_PATH/dockerBuild.sh" -r; bash "$SCRIPTS_PATH/dockerRun.sh"
+  fi
+}
+
+if [ "$IS_REMOTE" = "no" ] ; then
+  cd "servers/eureka-registry-server/" || return
+  BuildNRun
+  cd ../..
+  cd "servers/config-server/" || return
+  BuildNRun
+  cd ../..
+  echo; echo
+  echo "Waiting for the config server to be ready..."
+  sleep 10
+fi
+
+
 
 PrintSuccess "Starting all the services..."
 echo
 
-BuildNRun() {
-  bash "$SCRIPTS_PATH/dockerBuild.sh" -e "$EUREKA_IP_PORT" -s "$MYSQL_SERVER"
-  bash "$SCRIPTS_PATH/dockerRun.sh"
-}
-
 
 for d in */; do
-  if [ "${d: -8}" = "service/" ] && [ "$d" != "eureka-registry-service/" ] ; then
-    if [ "$d" = "delivery-proxy-service/" ] || [ "$d" = "payment-proxy-service/" ] ; then
+  if [ "${d: -8}" = "service/" ] ; then
+    echo; echo; echo; echo
+    PrintSuccess "Starting $d"
+    cd "$d" || return
+    BuildNRun
+    cd ..
+    echo; echo; echo; echo
+  else
+    if [ "${d: -8}" = "proxies/" ]; then
+      echo; echo; echo; echo
+      PrintSuccess "Starting proxies..."
+      echo
       cd "$d" || return
       for dd in */; do
         echo; echo; echo; echo
@@ -66,13 +96,6 @@ for d in */; do
         echo; echo; echo; echo
       done
       cd ..
-    else
-      echo; echo; echo; echo
-      PrintSuccess "Starting $d"
-      cd "$d" || return
-      BuildNRun
-      cd ..
-      echo; echo; echo; echo
     fi
   fi
 done
@@ -94,7 +117,11 @@ while : ; do
     PrintSuccess "Stopping all containers..."
     for d in */; do
       if [ "${d: -8}" = "service/" ] ; then
-        if [ "$d" = "delivery-proxy-service/" ] || [ "$d" = "payment-proxy-service/" ] ; then
+        cd "$d" || return
+        DockerStop
+        cd ..
+      else
+        if [ "${d: -8}" = "proxies/" ] ; then
           cd "$d" || return
           for dd in */; do
             cd "$dd" || return
@@ -102,13 +129,17 @@ while : ; do
             cd ..
           done
           cd ..
-        else
-          cd "$d" || return
-          DockerStop
-          cd ..
         fi
       fi
     done
+    if [ "$IS_REMOTE" = "no" ] ; then
+      cd "servers/eureka-registry-server/" || return
+      DockerStop
+      cd ../..
+      cd "servers/config-server/" || return
+      DockerStop
+      cd ../..
+    fi
     echo
     PrintSuccess "Exiting..."
     exit
--- managed-system/scripts/dockerBuild_configurable.sh
+++ managed-system/scripts/dockerBuild_configurable.sh
@@ -18,9 +18,7 @@ Help()
    echo "Syntax: dockerBuild [-OPTION]"
    echo
    echo "By default, this script:"
-   echo "- runs the microservice in the local mode."
-   echo "     Only the microservices hosted on the same LAN can access it"
-   echo "     All the server microservices, (i.e., the Eureka Server, the MySQL Server and the API Gateway) must be running locally too."
+   echo "- uses the private IP address of the machine. It means that only the microservices hosted on the same LAN can access it"
    echo "- makes the container expose the same port used by the microservice"
    echo "- creates a linux/arm64/v8 container."
    echo
@@ -28,10 +26,21 @@ Help()
    echo "h     Display help."
    echo "p     Specify port to be exposed by the container. Defaults to the port used by the service."
    echo "t     Specify target platform for the container. Defaults to linux/arm64/v8."
-   echo "r     Run the microservice in the remote mode."
-   echo "         It uses the public IP address of the machine, obtained through an external service. "
-   echo "         It usually requires a port forwarding for the microservice ports."
-   echo "         The addresses of the Eureka Server, of the MySQL Server and of the API Gateway are retrieved from the config repository"
+   echo "a     Specify the IP address of the machine hosting the container instance. "
+   echo "      The other microservices must be able to reach it at that IP address using the service port."
+   echo "r     Use the public IP address of the machine, obtained through an external service. "
+   echo "      It usually requires a port forwarding for the microservice ports."
+   #echo "i     Specify the IP address to assign to the container hosting the microservice. "
+   #echo "      If not specified, a free IP address will be used. "
+   #echo "      The IP must belong to the 172.0.0.0/16 subnet."
+   echo "e     Specify the IP address and the port of the Eureka Service, in the form IP:PORT. "
+   echo "      Defaults to the address specified in the application.properties if the -r option is used."
+   echo "      Otherwise, defaults to the private IP address of the hosting machine."
+   echo "s     Specify the IP address of the MySQL server. "
+   echo "      Defaults to the address specified in the application.properties if the -r option is used."
+   echo "      Otherwise, defaults to the private IP address of the hosting machine."
+   echo
+   echo "Use only one between -a and -r. When using -a or -r, the -e and -s options are mandatory."
 }
 
 # Get the service name from the application.properties file
@@ -48,18 +57,23 @@ if [ "$SERVICE_PORT" = "" ]; then
   exit 1
 fi
 
+# Get the Eureka IP and port from the application.properties file
+EUREKA_IP_PORT=`awk -v FS="EUREKA_IP_PORT=" 'NF>1{print $2}' ./src/main/resources/application.properties`
+if [ "$EUREKA_IP_PORT" = "" ]; then
+  PrintError "UNKNOWN EUREKA IP AND PORT. Make sure that EUREKA_IP_PORT is set in application.properties"
+  exit 1
+fi
+
 ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd && cd .. )"
 PORT_MAPPING="${SERVICE_PORT}:${SERVICE_PORT}"
 TARGET="linux/arm64/v8"
 
 # Use private IP address of the machine by default
 HOST=`ifconfig | grep '\<inet\>' | cut -d ' ' -f2 | grep -v '127.0.0.1'`
+EUREKA_IP_PORT="${HOST}:${EUREKA_IP_PORT##*:}"
 MYSQL_SERVER="${HOST}"
-EUREKA_IP_PORT="${HOST}:58082"
-API_GATEWAY_IP_PORT="${HOST}:58081"
-IS_REMOTE="no"
 
-while getopts "hrp:t:" option; do
+while getopts "hrp:t:a:e:s:" option; do
    case $option in
       h) # display Help
         Help
@@ -68,9 +82,16 @@ while getopts "hrp:t:" option; do
         PORT_MAPPING="${OPTARG}:${SERVICE_PORT}";;
       t) # Target
         TARGET="${OPTARG}";;
+      a) # Specify IP address of the machine hosting the container
+        HOST="${OPTARG}";;
       r) # Use public IP address of the machine
-        HOST="$(curl https://ipinfo.io/ip)"
-        IS_REMOTE="yes";;
+        HOST="$(curl https://ipinfo.io/ip)";;
+      #i) # Set IP address of the container
+      #  IP="--ip ${OPTARG}";;
+      e) # Set IP address and port of the Eureka service
+        EUREKA_IP_PORT="${OPTARG}";;
+      s) # Set IP address and port of the MySQL server hosting the databases
+        MYSQL_SERVER="${OPTARG}";;
      \?) # Wrong option
         PrintError "UNKNOWN OPTION $option"
         exit 1;;
@@ -103,13 +124,8 @@ echo ""
 echo "---------------------------------------------"
 
 echo ""
-if [ "$IS_REMOTE" = "no" ]; then
-  PrintSuccess "4/4 - Creating container $SERVICE_NAME in local mode"
-  docker create -p $PORT_MAPPING -e HOST=$HOST -e EUREKA_IP_PORT=$EUREKA_IP_PORT -e API_GATEWAY_IP_PORT=$API_GATEWAY_IP_PORT -e MYSQL_SERVER=$MYSQL_SERVER -i -t --name $SERVICE_NAME $SERVICE_NAME
-else
-  PrintSuccess "4/4 - Creating container $SERVICE_NAME in remote mode"
-  docker create -p $PORT_MAPPING -e HOST=$HOST -i -t --name $SERVICE_NAME $SERVICE_NAME
-fi
+PrintSuccess "4/4 - Creating container $SERVICE_NAME"
+docker create -p $PORT_MAPPING -e HOST=$HOST -e EUREKA_IP_PORT=$EUREKA_IP_PORT -e MYSQL_SERVER=$MYSQL_SERVER -i -t --name $SERVICE_NAME $SERVICE_NAME
 PrintSuccess "4/4 - DONE"
 echo ""
 
