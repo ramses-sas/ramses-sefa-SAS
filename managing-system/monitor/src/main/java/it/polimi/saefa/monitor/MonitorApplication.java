b8908cffd8cc6b55c7170fb7eca2be5af4a3d2d1
Giancarlo Sorrentino <57813140+SirGian99@users.noreply.github.com>
2022-08-30 17:22:39 +0200

    Periodic status checker

--- managing-system/monitor/src/main/java/it/polimi/saefa/monitor/MonitorApplication.java
+++ managing-system/monitor/src/main/java/it/polimi/saefa/monitor/MonitorApplication.java
@@ -3,6 +3,7 @@ package it.polimi.saefa.monitor;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
+import io.swagger.v3.core.util.Json;
 import it.polimi.saefa.knowledge.persistence.InstanceMetrics;
 import it.polimi.saefa.monitor.externalinterfaces.KnowledgeClient;
 import it.polimi.saefa.monitor.prometheus.PrometheusParser;
@@ -12,10 +13,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.http.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -50,7 +54,7 @@ public class MonitorApplication {
     }
 
 
-    @Scheduled(fixedDelay = 10_000) //delay in milliseconds
+    @Scheduled(fixedDelay = 100_000) //delay in milliseconds
     public void scheduleFixedDelayTask() {
         Map<String, List<InstanceInfo>> services = getServicesInstances();
         //log.debug("Services: {}", services);
@@ -67,9 +71,36 @@ public class MonitorApplication {
         });
     }
 
-
+    @Scheduled(fixedDelay = 50_000) //delay in milliseconds
+    public void periodicCheckDownStatus() {
+        Map<String, List<InstanceInfo>> services = getServicesInstances();
+        services.forEach((serviceName, serviceInstances) -> {
+            serviceInstances.forEach(instance -> {
+                if(getServicesInstances().get(serviceName).contains(instance)) {
+                    //this check is done to avoid the case in which the instance is gracefully
+                    // disconnected from eureka since the original services list is not updated.
+                    // Still, it can happen between the check and the actual call to the service, so how to solve?
+                    String url = instance.getHomePageUrl() + "actuator/health";
+                    try {
+                        ResponseEntity<Object> response = new RestTemplate().getForEntity(url, Object.class);
+                        String status = response.getBody().toString();
+                        if (!status.contains("status=UP"))
+                            throw new RuntimeException("Instance is down");
+                        else
+                            log.debug("Instance {} of service {} is up", instance.getHostName() + ":" + instance.getPort(), instance.getAppName());
+                    } catch (Exception e) {
+                        log.warn("Instance {} is down", url);
+                        InstanceMetrics instanceMetrics = new InstanceMetrics(instance.getAppName(), instance.getInstanceId());
+                        instanceMetrics.setUp(false);
+                        instanceMetrics.applyTimestamp();
+                        knowledgeClient.addMetrics(instanceMetrics);
+                    }
+                }
+            });
+        });
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
     }
-
 }
