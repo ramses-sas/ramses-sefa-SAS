e8f89b9cffee4765e06481549b7933d2330fafb0
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-08-30 11:20:02 +0200

    Update MetricsRepository.java

--- managing-system/knowledge/src/main/java/it/polimi/saefa/knowledge/persistence/MetricsRepository.java
+++ managing-system/knowledge/src/main/java/it/polimi/saefa/knowledge/persistence/MetricsRepository.java
@@ -1,6 +1,5 @@
 package it.polimi.saefa.knowledge.persistence;
 
-import it.polimi.saefa.knowledge.persistence.components.CircuitBreakerMetrics;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,12 +9,16 @@ import java.util.Date;
 
 public interface MetricsRepository extends CrudRepository<InstanceMetrics, Long> {
     Collection<InstanceMetrics> findAllByInstanceId(String instanceId);
+
     Collection<InstanceMetrics> findAllByServiceId(String serviceId);
+
     Collection<InstanceMetrics> findAllByTimestampBetween(Date start, Date end);
 
     InstanceMetrics findByInstanceIdAndTimestamp(String instanceId, Date timestamp);
+
     @Query("SELECT m FROM InstanceMetrics m WHERE m.instanceId = :instanceId AND m.timestamp = (SELECT MAX(m2.timestamp) FROM InstanceMetrics m2 WHERE m2.instanceId = :instanceId)")
     InstanceMetrics findLatestByInstanceId(@Param("instanceId") String instanceId);
+
     @Query("SELECT m FROM InstanceMetrics m WHERE m.serviceId = :serviceId AND m.timestamp = (SELECT MAX(m2.timestamp) FROM InstanceMetrics m2 WHERE m2.serviceId = :serviceId and m2.instanceId = m.instanceId)")
     Collection<InstanceMetrics> findLatestByServiceId(String serviceId);
 
