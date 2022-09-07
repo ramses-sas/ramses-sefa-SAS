f2bb9e8a0d6251c226a3b1f302beb9425daf36ff
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-09-07 15:19:01 +0200

    Update DashboardWebController.java

--- managing-system/dashboard/src/main/java/it/polimi/saefa/dashboard/adapters/DashboardWebController.java
+++ managing-system/dashboard/src/main/java/it/polimi/saefa/dashboard/adapters/DashboardWebController.java
@@ -1,11 +1,14 @@
 package it.polimi.saefa.dashboard.adapters;
 
 import it.polimi.saefa.dashboard.domain.DashboardWebService;
+import it.polimi.saefa.knowledge.persistence.domain.Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+
+import java.util.Map;
 
 @Controller
 @Slf4j
@@ -16,7 +19,8 @@ public class DashboardWebController {
 
 	/* Mostra home page */
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("currentConfiguration", dashboardWebService.getConfiguration().values().stream().toList());
 		return "index";
 	}
 
