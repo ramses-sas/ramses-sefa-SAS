5936232dae6e73f7e43c0cf4c215c05f3d28d4de
Giancarlo Sorrentino <57813140+SirGian99@users.noreply.github.com>
2022-09-12 16:01:04 +0200

    basic config

--- managing-system/analyze/src/main/java/it/polimi/saefa/analyze/AnalyzeApplication.java
+++ managing-system/analyze/src/main/java/it/polimi/saefa/analyze/AnalyzeApplication.java
@@ -1,15 +1,13 @@
-package it.polimi.saefa.knowledge;
+package it.polimi.saefa.analyze;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
 @SpringBootApplication
 @EnableDiscoveryClient
-public class KnowledgeApplication {
+public class AnalyzeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(KnowledgeApplication.class, args);
+        SpringApplication.run(AnalyzeApplication.class, args);
     }
-
 }
---
+++ managing-system/analyze/src/main/java/it/polimi/saefa/analyze/domain/AnalyzeService.java
@@ -0,0 +1,7 @@
+package it.polimi.saefa.analyze.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnalyzeService {
+}
