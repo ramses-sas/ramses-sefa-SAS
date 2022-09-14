41780589405d0633fed05f3fb4bad67ef911f10d
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-09-14 11:57:30 +0200

    Instance and Service Stats

--- managing-system/analyse/src/main/java/it/polimi/saefa/analyse/domain/AnalyseService.java
+++ managing-system/analyse/src/main/java/it/polimi/saefa/analyse/domain/AnalyseService.java
@@ -1,58 +1,66 @@
 package it.polimi.saefa.analyse.domain;
 
 import it.polimi.saefa.analyse.externalInterfaces.KnowledgeClient;
-import it.polimi.saefa.knowledge.persistence.domain.adaptation.AdaptationParameter;
 import it.polimi.saefa.knowledge.persistence.domain.architecture.Instance;
 import it.polimi.saefa.knowledge.persistence.domain.architecture.Service;
-import it.polimi.saefa.knowledge.persistence.domain.metrics.HttpRequestMetrics;
 import it.polimi.saefa.knowledge.persistence.domain.metrics.InstanceMetrics;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.swing.text.DateFormatter;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.*;
 
 @org.springframework.stereotype.Service
 public class AnalyseService {
-    //Sliding window con tot nuove e tot vecchie
     private Date lastAnalysisTimestamp = Date.from(Instant.MIN);
     private final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-    private int analysisWindow = 30;
-    private int analysisWindowStep = 20; // 10 metriche vecchie, 20 metriche nuove
+
+    //Sliding window of size `analysisWindowSize`, containing `analysisWindowStep` new metrics
+    private final int analysisWindowSize = 30;
+    private final int analysisWindowStep = 20; // 10 metriche vecchie, 20 metriche nuove
+
     @Autowired
     private KnowledgeClient knowledgeClient;
-    public void startAnalysis() {
 
-        //ci serve il set di servizi per ottenere la cofigurazione attuale
+    public void startAnalysis() {
         List<Service> currentServices = knowledgeClient.getServices();
-        for(Service service : currentServices) {
-            for(Instance instance : service.getInstances()){
+        for (Service service : currentServices) {
+            ServiceStats serviceStats = new ServiceStats(service);
+            // Analyze all the instances
+            for (Instance instance : service.getInstances()) {
                 List<InstanceMetrics> metrics = new LinkedList<>();
                 int afterMetrics = analysisWindowStep;
-                metrics.addAll(knowledgeClient.getLatestNMetricsBeforeDate(instance.getInstanceId(), dateFormatter.format(lastAnalysisTimestamp), analysisWindow - analysisWindowStep));
-                if(metrics.size() != analysisWindow - analysisWindowStep){
-                    afterMetrics = analysisWindow - metrics.size();
+                metrics.addAll(knowledgeClient.getLatestNMetricsBeforeDate(instance.getInstanceId(), dateFormatter.format(lastAnalysisTimestamp), analysisWindowSize - analysisWindowStep));
+                if (metrics.size() != analysisWindowSize - analysisWindowStep) {
+                    afterMetrics = analysisWindowSize - metrics.size();
                 }
                 metrics.addAll(knowledgeClient.getLatestNMetricsAfterDate(instance.getInstanceId(), dateFormatter.format(lastAnalysisTimestamp), afterMetrics));
 
-                if(metrics.size() != analysisWindow){
+                // Not enough data to perform analysis
+                if (metrics.size() != analysisWindowSize) {
                     return; //TODO gestire
                 }
                 metrics.sort(Comparator.comparing(InstanceMetrics::getTimestamp));
-
-                Map<String, Double> endpointAvgRespTime = new HashMap<>();
-                Map<String, Double> endpointMaxRespTime = new HashMap<>();
                 InstanceMetrics firstMetrics = metrics.get(0);
                 InstanceMetrics lastMetrics = metrics.get(metrics.size() - 1);
 
-                for(String endpoint : firstMetrics.getHttpMetrics().keySet()){
+                // <endpoint, value>
+                Map<String, Double> endpointAvgRespTime = new HashMap<>();
+                // <endpoint, value>
+                Map<String, Double> endpointMaxRespTime = new HashMap<>();
+
+                for (String endpoint : firstMetrics.getHttpMetrics().keySet()) {
                     double durationDifference = lastMetrics.getHttpMetrics().get(endpoint).getTotalDuration() - firstMetrics.getHttpMetrics().get(endpoint).getTotalDuration();
                     double requestDifference = lastMetrics.getHttpMetrics().get(endpoint).getCount() - firstMetrics.getHttpMetrics().get(endpoint).getCount();
-                    endpointAvgRespTime.put(endpoint, durationDifference/requestDifference);
+                    if (requestDifference != 0)
+                        endpointAvgRespTime.put(endpoint, durationDifference / requestDifference);
                     endpointMaxRespTime.put(endpoint, lastMetrics.getHttpMetrics().get(endpoint).getMaxDuration());
                 }
+
+                // Single instance statistics (i.e., values that the analysis use to compute adaptation options)
+                InstanceStats instanceStats = new InstanceStats(instance, computeAvgResponseTime(endpointAvgRespTime),
+                        computeMaxResponseTime(endpointMaxRespTime), computeAvailability(metrics));
+                serviceStats.addInstanceStats(instanceStats);
             }
 
         }
@@ -60,16 +68,16 @@ public class AnalyseService {
         //creare lista di liste di servizi da mandare al pranner
     }
 
-    private double computeAvailability(Service service) {
-        return 0;
+    private Double computeAvailability(List<InstanceMetrics> metrics) {
+        return null;
     }
 
-    private double computeAverageResponseTime(Service service) {
-        return 0;
+    private Double computeMaxResponseTime(Map<String, Double> endpointMaxRespTime) {
+        return endpointMaxRespTime.values().stream().max(Double::compareTo).orElse(null);
     }
 
-    private double computeMaxResponseTime(Service service) {
-        return 0;
+    private Double computeAvgResponseTime(Map<String, Double> endpointAvgRespTime) {
+        return endpointAvgRespTime.values().stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
     }
 
 
---
+++ managing-system/analyse/src/main/java/it/polimi/saefa/analyse/domain/ServiceStats.java
@@ -0,0 +1,52 @@
+package it.polimi.saefa.analyse.domain;
+
+import it.polimi.saefa.knowledge.persistence.domain.architecture.Service;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ServiceStats {
+    private Service service;
+    private List<InstanceStats> instancesStats = new ArrayList<>();
+    private Double averageResponseTime;
+    private Double maxResponseTime;
+    private Double availability;
+
+    public ServiceStats(Service service) {
+        this.service = service;
+    }
+
+    public String getServiceId() {
+        return service.getServiceId();
+    }
+
+    public void addInstanceStats(InstanceStats instanceStats) {
+        instancesStats.add(instanceStats);
+    }
+
+    public void updateAverageResponseTime() {
+        if (instancesStats.isEmpty())
+            averageResponseTime = null;
+        averageResponseTime = instancesStats.stream().mapToDouble(InstanceStats::getAverageResponseTime).average().orElseThrow();
+    }
+
+    public void updateMaxResponseTime() {
+        if (instancesStats.isEmpty())
+            maxResponseTime = null;
+        maxResponseTime = instancesStats.stream().mapToDouble(InstanceStats::getMaxResponseTime).max().orElseThrow();
+    }
+
+    public void updateAvailability() {
+        if (instancesStats.isEmpty())
+            availability = null;
+        availability = instancesStats.stream().mapToDouble(InstanceStats::getAvailability).reduce(1.0, (accumulator, val) -> accumulator * (1-val));
+    }
+}
