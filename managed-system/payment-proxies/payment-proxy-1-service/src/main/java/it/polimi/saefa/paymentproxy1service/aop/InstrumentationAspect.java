22f7bdc158ba841ce704f418ff49b2d2b0c6bb99
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-09-25 17:56:11 +0200

    Instrumentation Aspect

--- managed-system/payment-proxies/payment-proxy-1-service/src/main/java/it/polimi/saefa/paymentproxy1service/aop/InstrumentationAspect.java
+++ managed-system/payment-proxies/payment-proxy-1-service/src/main/java/it/polimi/saefa/paymentproxy1service/aop/InstrumentationAspect.java
@@ -1,16 +1,32 @@
-package it.polimi.saefa.paymentproxy1service.aoplog;
+package it.polimi.saefa.paymentproxy1service.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Random;
 
 @Component
 @Aspect
 @Slf4j
-public class PaymentProxyServiceLoggingAspect {
+public class InstrumentationAspect {
+    private final Double sleepMean;
+    private final Double sleepVariance;
+    private final Double exceptionProbability;
+
+    public InstrumentationAspect(Environment env) {
+        String sleepMean, sleepVariance, exceptionProbability;
+        sleepMean = env.getProperty("SLEEP_MEAN");
+        sleepVariance = env.getProperty("SLEEP_VARIANCE");
+        exceptionProbability = env.getProperty("EXCEPTION_PROBABILITY");
+        this.sleepMean = sleepMean == null ? null : Double.parseDouble(sleepMean);
+        this.sleepVariance = sleepVariance == null ? null : Double.parseDouble(sleepVariance);
+        this.exceptionProbability = exceptionProbability == null ? null : Double.parseDouble(exceptionProbability);
+    }
+
 
     /* Pointcut per il servizio dei ristoranti */
     @Pointcut("execution(public * it.polimi.saefa.paymentproxy1service.domain.PaymentProxyService.*(..))")
@@ -23,42 +39,51 @@ public class PaymentProxyServiceLoggingAspect {
     private void logInvocation(JoinPoint joinPoint) {
         final String args = Arrays.toString(joinPoint.getArgs());
         final String methodName = joinPoint.getSignature().getName().replace("(..)", "()");
-        log.info("CALL PaymentProxyService from proxy 1.{} {}", methodName, args);
+        log.info("CALL PaymentProxyService.{} {}", methodName, args);
     }
 
     private void logTermination(JoinPoint joinPoint, Object retValue) {
         final String args = Arrays.toString(joinPoint.getArgs());
         final String methodName = joinPoint.getSignature().getName().replace("(..)", "()");
-        log.info("     PaymentProxyService from proxy 1.{} {} -> {}", methodName, args, retValue.toString());
+        log.info("     PaymentProxyService.{} {} -> {}", methodName, args, retValue.toString());
     }
 
     private void logVoidTermination(JoinPoint joinPoint) {
         final String args = Arrays.toString(joinPoint.getArgs());
         final String methodName = joinPoint.getSignature().getName().replace("(..)", "()");
-        log.info("     PaymentProxyService from proxy 1.{} {} -> RETURN", methodName, args);
+        log.info("     PaymentProxyService.{} {} -> RETURN", methodName, args);
     }
 
     private void logException(JoinPoint joinPoint, Object exception) {
         final String args = Arrays.toString(joinPoint.getArgs());
         final String methodName = joinPoint.getSignature().getName().replace("(..)", "()");
-        log.info("     ERROR IN PaymentProxyService from proxy 1.{} {} -> {}", methodName, args, exception.toString());
+        log.info("     ERROR IN PaymentProxyService.{} {} -> {}", methodName, args, exception.toString());
     }
 
     /* Eseguito prima dell'esecuzione del metodo */
     @Before("paymentProxyServiceMethods()")
     public void logBeforeExecuteMethod(JoinPoint joinPoint) {
+        try {
+            long sleepTime = generateSleep();
+            log.debug(""+sleepTime);
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {}
         logInvocation(joinPoint);
     }
 
     /* Eseguito quando il metodo è terminato (con successo) */
     @AfterReturning(value="paymentProxyServiceMethods() &&! paymentProxyServiceVoidMethods()", returning="retValue")
     public void logSuccessMethod(JoinPoint joinPoint, Object retValue) {
+        // Throw an exception with a certain probability
+        shouldThrowException();
         logTermination(joinPoint, retValue);
     }
 
     /* Eseguito quando il metodo (void) è terminato (con successo) */
     @AfterReturning("paymentProxyServiceVoidMethods()")
     public void logSuccessVoidMethod(JoinPoint joinPoint) {
+        // Throw an exception with a certain probability
+        shouldThrowException();
         logVoidTermination(joinPoint);
     }
 
@@ -66,6 +91,17 @@ public class PaymentProxyServiceLoggingAspect {
     @AfterThrowing(value="paymentProxyServiceMethods()", throwing="exception")
     public void logErrorApplication(JoinPoint joinPoint, Exception exception) {
         logException(joinPoint, exception);
+    }
+
+    private long generateSleep() {
+        if (sleepMean == null || sleepVariance == null)
+            return 0;
+        return (long)((new Random()).nextGaussian()*sleepVariance + sleepMean);
+    }
+
+    private void shouldThrowException() throws RuntimeException {
+        if (exceptionProbability != null && (new Random()).nextDouble() < exceptionProbability)
+            throw new RuntimeException("An artificial exception has been thrown!");
     }
 
 }
--- managed-system/restaurant-service/src/main/java/it/polimi/saefa/restaurantservice/aop/InstrumentationAspect.java
+++ managed-system/restaurant-service/src/main/java/it/polimi/saefa/restaurantservice/aop/InstrumentationAspect.java
@@ -1,16 +1,33 @@
-package it.polimi.saefa.restaurantservice.aoplog;
+package it.polimi.saefa.restaurantservice.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Random;
 
 @Component
 @Aspect
 @Slf4j
-public class RestaurantServiceLoggingAspect {
+public class InstrumentationAspect {
+    private final Double sleepMean;
+    private final Double sleepVariance;
+    private final Double exceptionProbability;
+
+    public InstrumentationAspect(Environment env) {
+        String sleepMean, sleepVariance, exceptionProbability;
+        sleepMean = env.getProperty("SLEEP_MEAN");
+        sleepVariance = env.getProperty("SLEEP_VARIANCE");
+        exceptionProbability = env.getProperty("EXCEPTION_PROBABILITY");
+        this.sleepMean = sleepMean == null ? null : Double.parseDouble(sleepMean);
+        this.sleepVariance = sleepVariance == null ? null : Double.parseDouble(sleepVariance);
+        this.exceptionProbability = exceptionProbability == null ? null : Double.parseDouble(exceptionProbability);
+    }
+
 
     /* Pointcut per il servizio dei ristoranti */
     @Pointcut("execution(public * it.polimi.saefa.restaurantservice.domain.RestaurantService.*(..))")
@@ -47,18 +64,27 @@ public class RestaurantServiceLoggingAspect {
     /* Eseguito prima dell'esecuzione del metodo */
     @Before("restaurantServiceMethods()")
     public void logBeforeExecuteMethod(JoinPoint joinPoint) {
+        try {
+            long sleepTime = generateSleep();
+            log.debug(""+sleepTime);
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {}
         logInvocation(joinPoint);
     }
 
     /* Eseguito quando il metodo è terminato (con successo) */
     @AfterReturning(value="restaurantServiceMethods() &&! restaurantServiceVoidMethods()", returning="retValue")
     public void logSuccessMethod(JoinPoint joinPoint, Object retValue) {
+        // Throw an exception with a certain probability
+        shouldThrowException();
         logTermination(joinPoint, retValue);
     }
 
     /* Eseguito quando il metodo (void) è terminato (con successo) */
     @AfterReturning("restaurantServiceVoidMethods()")
     public void logSuccessVoidMethod(JoinPoint joinPoint) {
+        // Throw an exception with a certain probability
+        shouldThrowException();
         logVoidTermination(joinPoint);
     }
 
@@ -66,6 +92,17 @@ public class RestaurantServiceLoggingAspect {
     @AfterThrowing(value="restaurantServiceMethods()", throwing="exception")
     public void logErrorApplication(JoinPoint joinPoint, Exception exception) {
         logException(joinPoint, exception);
+    }
+
+    private long generateSleep() {
+        if (sleepMean == null || sleepVariance == null)
+            return 0;
+        return (long)((new Random()).nextGaussian()*sleepVariance + sleepMean);
+    }
+
+    private void shouldThrowException() throws RuntimeException {
+        if (exceptionProbability != null && (new Random()).nextDouble() < exceptionProbability)
+            throw new RuntimeException("An artificial exception has been thrown!");
     }
 
 }
