f37fcd3ee5cfc9e600a4729aa36615af0ac2dd78
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-10-25 21:58:44 +0200

    RestClient for simulations + sql statements

---
+++ managing-system/dropTables.sql
@@ -0,0 +1,7 @@
+-- /usr/local/mysql/bin/mysql --user=saefauser --password=SaefaPW0! < dropTables.sql
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE `knowledge`.`adaptation_option`, `knowledge`.`add_instance_option`, `knowledge`.`add_instance_option_instances_to_shutdown_ids`, `knowledge`.`add_instance_option_old_instances_new_weights`, `knowledge`.`change_implementation_option`, `knowledge`.`change_implementation_option_possible_implementations`, `knowledge`.`change_load_balancer_weights_option`, `knowledge`.`change_load_balancer_weights_option_instances_to_shutdown_ids`, `knowledge`.`change_load_balancer_weights_option_new_weights`, `knowledge`.`circuit_breaker_metrics`, `knowledge`.`circuit_breaker_metrics_buffered_calls_count`, `knowledge`.`circuit_breaker_metrics_call_count`, `knowledge`.`circuit_breaker_metrics_call_duration`, `knowledge`.`circuit_breaker_metrics_call_max_duration`, `knowledge`.`circuit_breaker_metrics_slow_call_count`, `knowledge`.`http_endpoint_metrics`, `knowledge`.`http_endpoint_metrics_outcome_metrics`, `knowledge`.`instance_metrics_snapshot`, `knowledge`.`instance_metrics_snapshot_circuit_breaker_metrics`, `knowledge`.`instance_metrics_snapshot_http_metrics`, `knowledge`.`qosvalue_entity`, `knowledge`.`service_configuration`, `knowledge`.`service_configuration_circuit_breakers_configuration`, `knowledge`.`service_configuration_load_balancer_weights`, `knowledge`.`shutdown_instance_option`, `knowledge`.`shutdown_instance_option_new_weights`;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- rest-client/src/main/java/it/polimi/saefa/restclient/RequestsGenerator.java
+++ rest-client/src/main/java/it/polimi/saefa/restclient/RequestsGenerator.java
@@ -3,51 +3,137 @@ package it.polimi.saefa.restclient;
 import it.polimi.saefa.orderingservice.restapi.AddItemToCartResponse;
 import it.polimi.saefa.orderingservice.restapi.CartItemElement;
 import it.polimi.saefa.orderingservice.restapi.ConfirmOrderResponse;
+import it.polimi.saefa.orderingservice.restapi.CreateCartResponse;
 import it.polimi.saefa.restaurantservice.restapi.common.*;
+import it.polimi.saefa.restclient.domain.AdaptationController;
 import it.polimi.saefa.restclient.domain.RequestGeneratorService;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledFuture;
 
 @Slf4j
 @Component
+@Data
 public class RequestsGenerator {
+    @Value("${SCHEDULING_PERIOD}")
+    private long schedulingPeriod;
+    @Value("${ADAPT}")
+    private int adapt;
 
     @Autowired
-    RequestGeneratorService requestGeneratorService;
+    private RequestGeneratorService requestGeneratorService;
+    @Autowired
+    private AdaptationController adaptationController;
+    private ThreadPoolTaskScheduler taskScheduler;
+    private ScheduledFuture<?> simulationRoutine;
 
-    public void simulateOrder() {
-        Collection<GetRestaurantResponse> restaurants = requestGeneratorService.getAllRestaurants();
-        assert restaurants.size() >= 1;
-        GetRestaurantResponse restaurant = restaurants.iterator().next();
-        long restaurantId = restaurant.getId();
-        GetRestaurantMenuResponse menu = requestGeneratorService.getRestaurantMenu(restaurantId);
-        assert menu.getMenuItems().size() >= 1;
-        MenuItemElement menuItem = menu.getMenuItems().iterator().next();
-        long cartId = requestGeneratorService.createCart(restaurantId).getId();
-        AddItemToCartResponse cart = requestGeneratorService.addItemToCart(cartId, restaurantId, menuItem.getId(), 2);
-        assert  cart.getItems().size() == 1;
-        CartItemElement returnedItem = cart.getItems().iterator().next();
-        assert  returnedItem.getQuantity() == 2 &&
-                returnedItem.getId().equals(menuItem.getId()) &&
-                returnedItem.getName().equals(menuItem.getName()) &&
-                cart.getTotalPrice() == menuItem.getPrice() * 2;
-        ConfirmOrderResponse confirmedOrder = requestGeneratorService.confirmOrder(cartId, "1111111111111111", 12, 2023, "001",
-                "Via REST Client", "Roma", 1, "12345", "1234567890", new Date());
-        if (!confirmedOrder.isConfirmed()) {
-            if (confirmedOrder.getRequiresCashPayment()) {
-                log.info("Order confirmed, but requires cash payment");
-                confirmedOrder = requestGeneratorService.confirmCashPayment(cartId, "Via REST Client", "Roma", 1, "12345", "1234567890", new Date());
-            }
-            if (!confirmedOrder.isConfirmed() && confirmedOrder.getIsTakeAway()) {
-                log.info("Order confirmed, but requires take away");
-                confirmedOrder = requestGeneratorService.handleTakeAway(cartId, true);
-            }
-        }
-        assert confirmedOrder.isConfirmed();
-        log.info("Order confirmed!");
+    public RequestsGenerator(ThreadPoolTaskScheduler taskScheduler) {
+        this.taskScheduler = taskScheduler;
     }
+
+    @PostConstruct
+    public void init() {
+        startSimulation();
+
+        // Start Monitor and enable adaptation if ADAPT env var is != 0 after 10 seconds
+        TimerTask startManagingTask = new TimerTask() {
+            public void run() {
+                log.info("Starting Monitor Routine");
+                adaptationController.startMonitorRoutine();
+                if (adapt != 0) {
+                    log.info("Enabling adaptation");
+                    adaptationController.changeAdaptationStatus(true);
+                } else {
+                    log.info("Disabling adaptation");
+                    adaptationController.changeAdaptationStatus(false);
+                }
+            }
+        };
+        Timer startManagingTimer = new Timer("StartManagingTimer");
+        startManagingTimer.schedule(startManagingTask, 1000*10);
+
+        // Stop simulation after 20 minutes
+        TimerTask stopSimulationTask = new TimerTask() {
+            public void run() {
+                log.info("Stopping simulation");
+                stopSimulation();
+                log.info("Stopping Monitor Routine");
+                adaptationController.stopMonitorRoutine();
+                log.info("Disabling adaptation");
+                adaptationController.changeAdaptationStatus(false);
+                System.exit(0);
+            }
+        };
+        Timer stopSimulationTimer = new Timer("StopSimulationTimer");
+        stopSimulationTimer.schedule(stopSimulationTask, 1000*60*20);
+    }
+
+    class SimulationRoutine implements Runnable {
+        @Override
+        public void run() {
+            Collection<GetRestaurantResponse> restaurants = requestGeneratorService.getAllRestaurants();
+            if (restaurants == null || restaurants.size() < 1) throw new RuntimeException("No restaurants available");
+            GetRestaurantResponse restaurant = restaurants.iterator().next();
+            long restaurantId = restaurant.getId();
+            GetRestaurantMenuResponse menu = requestGeneratorService.getRestaurantMenu(restaurantId);
+            if (menu == null || menu.getMenuItems().size() < 1) throw new RuntimeException("No menu items available");
+            MenuItemElement menuItem = menu.getMenuItems().iterator().next();
+            CreateCartResponse cartCreated = requestGeneratorService.createCart(restaurantId);
+            if (cartCreated == null) throw new RuntimeException("Cart creation failed");
+            long cartId = cartCreated.getId();
+            AddItemToCartResponse cart = requestGeneratorService.addItemToCart(cartId, restaurantId, menuItem.getId(), 2);
+            if (cart == null || cart.getItems().size() != 1) throw new RuntimeException("Wrong number of items in cart");
+            CartItemElement returnedItem = cart.getItems().iterator().next();
+            if (returnedItem.getQuantity() != 2 ||
+                    !returnedItem.getId().equals(menuItem.getId()) ||
+                    !returnedItem.getName().equals(menuItem.getName()) ||
+                    cart.getTotalPrice() != menuItem.getPrice() * 2) throw new RuntimeException("Inconsistent cart");
+            ConfirmOrderResponse confirmedOrder = requestGeneratorService.confirmOrder(cartId, "1111111111111111", 12, 2023, "001",
+                    "Via REST Client", "Roma", 1, "12345", "1234567890", new Date());
+            if (confirmedOrder == null) throw new RuntimeException("Impossible to confirm order [1]");
+            if (!confirmedOrder.isConfirmed()) {
+                if (confirmedOrder.getRequiresCashPayment()) {
+                    log.info("Order confirmed, but requires cash payment");
+                    confirmedOrder = requestGeneratorService.confirmCashPayment(cartId, "Via REST Client", "Roma", 1, "12345", "1234567890", new Date());
+                }
+                if (confirmedOrder == null) throw new RuntimeException("Impossible to confirm order [2]");
+                if (!confirmedOrder.isConfirmed() && confirmedOrder.getIsTakeAway()) {
+                    log.info("Order confirmed, but requires take away");
+                    confirmedOrder = requestGeneratorService.handleTakeAway(cartId, true);
+                }
+                if (confirmedOrder == null) throw new RuntimeException("Impossible to confirm order [3]");
+            }
+            if (!confirmedOrder.isConfirmed()) throw new RuntimeException("Order not confirmed");
+            log.info("Order confirmed!");
+        }
+    }
+
+    public void startSimulation() {
+        if (simulationRoutine == null || simulationRoutine.isCancelled()) {
+            log.info("simulationRoutine starting");
+            simulationRoutine = taskScheduler.scheduleAtFixedRate(new SimulationRoutine(), schedulingPeriod);
+        } else {
+            log.info("simulationRoutine already running");
+        }
+    }
+
+    public void stopSimulation() {
+        if (simulationRoutine.cancel(false)) {
+            log.info("simulationRoutine stopping");
+            simulationRoutine = null;
+        } else {
+            log.error("Error stopping simulationRoutine");
+        }
+    }
+
 }
