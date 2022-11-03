3f19f50c9118eecc3e202574f4ba64fea06cdf73
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2022-11-03 12:15:18 +0100

    refactor + dummy managed with continuous graph

--- managed-system/simulations/OLDgenerateGraphsPoints.py
+++ managed-system/simulations/OLDgenerateGraphsPoints.py
@@ -37,11 +37,10 @@ try:
                         thresholds.append(row[3])
                         invalidates.append(row[4])
                         timestamps.append(row[5])
-                    min_timestamp = timestamps[0].timestamp()
-                    values_coordinates = [((timestamps[i].timestamp() - min_timestamp)/60, values[i]) for i in range(len(values))]
-                    thresholds_coordinates = [((timestamps[i].timestamp() - min_timestamp)/60, thresholds[i]) for i in range(len(thresholds))]
-                    current_values_coordinates = [((timestamps[i].timestamp() - min_timestamp)/60, current_values[i]) for i in range(len(current_values)) if current_values[i] is not None]
-                    invalidates_coordinates = [((timestamps[i].timestamp() - min_timestamp)/60, values[i]) for i in range(len(invalidates)) if invalidates[i] == 1]
+                    values_coordinates = [(i+1, values[i]) for i in range(len(values))]
+                    thresholds_coordinates = [(i+1, thresholds[i]) for i in range(len(thresholds))]
+                    current_values_coordinates = [(i+1, current_values[i]) for i in range(len(current_values)) if current_values[i] is not None]
+                    invalidates_coordinates = [(i+1, values[i]) for i in range(len(invalidates)) if invalidates[i] == 1]
 
                     adapt_query = "SELECT a.discriminator, a.service_implementation_id, a.timestamp FROM adaptation_option a"
                     adapt_query += " WHERE a.service_id = '"+service+"' ORDER BY a.timestamp ASC"
@@ -63,7 +62,7 @@ try:
                     for i in range(len(option_timestamps)):
                         for j in range(len(timestamps)-1):
                             if option_timestamps[i] >= timestamps[j] and option_timestamps[i] < timestamps[j+1] and option_implementation_ids[i] == implementation_ids[j]:
-                                adaptation_coordinates.append((timestamps[j].timestamp() - min_timestamp)/60)
+                                adaptation_coordinates.append(j+1)
                                 break           
                     
                     # adaptation_coordinates = [(i+1) for i in range(len(values)) if timestamps[i] in option_timestamps and implementation_ids[i] in option_implementation_ids]
@@ -72,18 +71,18 @@ try:
                     for file_to_generate in files_to_generate:
                         var_name = [ k for k,v in locals().items() if v is file_to_generate][0]
                         os.makedirs(FILEPATH+"/"+service, exist_ok = True)
-                        with open(FILEPATH+"/"+service+"/"+qos+"_"+var_name.replace("_coordinates","")+".txt", "w") as f1:
-                            f1.writelines("i value\n")
+                        with open(FILEPATH+"/"+service+"/"+qos+"_"+var_name.replace("_coordinates","")+".txt", "w") as f:
+                            f.writelines("i value\n")
                             #print(var_name)
                             for val in file_to_generate:
                                 #print(val[0], val[1])
-                                f1.writelines(f'{val[0]:.3f}'+" "+f'{val[1]:.3f}'+"\n")
-                    with open(FILEPATH+"/"+service+"/"+qos+"_adaptation.txt", "w") as f2:
-                        f2.writelines("i\n")
+                                f.writelines(str(val[0])+" "+str(val[1])+"\n")
+                    with open(FILEPATH+"/"+service+"/"+qos+"_adaptation.txt", "w") as f:
+                        f.writelines("i\n")
                         #print("adaptation_coordinates")
                         for val in adaptation_coordinates:
                             #print(val)
-                            f2.writelines(f'{val:.3f}'+"\n")
+                            f.writelines(str(val)+"\n")
            
 except Error as e:
     print(e)
